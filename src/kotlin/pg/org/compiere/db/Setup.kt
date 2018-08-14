53e8a3b207f2756af7ccf3ac11771b67810a3b09
davidpodhola <david@podhola.net>
2018-08-14 22:01:55 +0200

    c3p0 -> Hikari

--- src/kotlin/pg/org/compiere/db/Setup.kt
+++ src/kotlin/pg/org/compiere/db/Setup.kt
@@ -1,14 +1,6 @@
 package pg.org.compiere.db
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
-import software.hsharp.api.icommon.ICConnection
-import software.hsharp.api.icommon.IDatabaseSetup
 import software.hsharp.db.postgresql.provider.PgDB
-import software.hsharp.db.postgresql.provider.PgDatabaseSetup
-import java.sql.Driver
-import java.sql.DriverManager
-import java.util.*
 
 open class PooledPgDB : PgDB() {
 }
